64a37ca91a8f9c96cbe2d93143ab14845533454f
Brett Ausmeier <brett@ausmeier.co.za>
2017-07-17 10:59:08 +0200

    Allow searching for Practitioners by identifier
    Support the identifier query parameter when searching for Practitioner
    resources. This change only allows for a single identifier to be
    supplied.

---
+++ test/builders/practitioner-query.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const buildPractitionerQuery = require('../../lib/builders/practitioner-query')
+const tap = require('tap')
+
+tap.test('Practitioner query builder', (t) => {
+  t.test('should match on resource type when there are no parameters', (t) => {
+    const expectedQuery = {
+      resourceType: 'Practitioner'
+    }
+    const query = buildPractitionerQuery({})
+    t.deepEqual(query, expectedQuery)
+    t.end()
+  })
+
+  t.test('should add a filter for the identifier query parameter', (t) => {
+    const expectedQuery = {
+      resourceType: 'Practitioner',
+      identifier: {
+        $elemMatch: {
+          value: '123'
+        }
+      }
+    }
+    const query = buildPractitionerQuery({
+      identifier: '123'
+    })
+    t.deepEqual(query, expectedQuery)
+    t.end()
+  })
+
+  t.test('should add a filter for the identifier query parameter with a namespace', (t) => {
+    const expectedQuery = {
+      resourceType: 'Practitioner',
+      identifier: {
+        $elemMatch: {
+          system: 'http://example.com',
+          value: '123'
+        }
+      }
+    }
+    const query = buildPractitionerQuery({
+      identifier: 'http://example.com|123'
+    })
+    t.deepEqual(query, expectedQuery)
+    t.end()
+  })
+
+  t.end()
+})
